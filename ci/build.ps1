52585f74f6b2ba762d18a2925b94075ad842f9f7
Tim Brooks <brooks@skoorb.net>
2023-12-18 01:18:26 +0000

    Add hidden-import for pyinstaller
    Trying to add winrt-Windows.Foundation.Collections to output.

--- ci/build.ps1
+++ ci/build.ps1
@@ -3,5 +3,5 @@ Push-Location ui
 npm run build
 Pop-Location
 
-pyinstaller --add-data "version.txt;." backend/main_server.py
+pyinstaller --add-data "version.txt;." --hidden-import=winrt-Windows.Foundation.Collections backend/main_server.py
 pyinstaller --add-data "ui/dist;ui" ui/serve.py
