417d447c91ec6624ce02a98f8ebb9600f4d422ff
Tim Brooks <brooks@skoorb.net>
2023-12-30 17:18:45 +0000

    Print windows DLLs on GH actions builder

--- ci/build.ps1
+++ ci/build.ps1
@@ -3,5 +3,7 @@ Push-Location ui
 npm run build
 Pop-Location
 
+ls C:\Windows\System32\downlevel
+
 pyinstaller --add-data "version.txt;." --paths C:\Windows\System32\downlevel backend/main_server.py
 pyinstaller --add-data "ui/dist;ui" ui/serve.py
